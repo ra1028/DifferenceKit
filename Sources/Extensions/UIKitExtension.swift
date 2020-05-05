e89a54154580db25c7ce507582a8e32b5170430d
Alexander Shoshiashvili <aleksandr.shoshiashvili@gmail.com>
2020-05-05 12:54:28 +0200

    Fix crash

--- Sources/Extensions/UIKitExtension.swift
+++ Sources/Extensions/UIKitExtension.swift
@@ -62,49 +62,52 @@ public extension UITableView {
         reloadRowsAnimation: @autoclosure () -> RowAnimation,
         interrupt: ((Changeset<C>) -> Bool)? = nil,
         setData: (C) -> Void
-        ) {
+    ) {
         if case .none = window, let data = stagedChangeset.last?.data {
             setData(data)
             return reloadData()
         }
-
+        
         for changeset in stagedChangeset {
             if let interrupt = interrupt, interrupt(changeset), let data = stagedChangeset.last?.data {
                 setData(data)
-                return reloadData()
+                reloadData()
+                return
             }
-
-            _performBatchUpdates {
+        }
+        
+        _performBatchUpdates {
+            for changeset in stagedChangeset {
                 setData(changeset.data)
-
+                
                 if !changeset.sectionDeleted.isEmpty {
                     deleteSections(IndexSet(changeset.sectionDeleted), with: deleteSectionsAnimation())
                 }
-
+                
                 if !changeset.sectionInserted.isEmpty {
                     insertSections(IndexSet(changeset.sectionInserted), with: insertSectionsAnimation())
                 }
-
+                
                 if !changeset.sectionUpdated.isEmpty {
                     reloadSections(IndexSet(changeset.sectionUpdated), with: reloadSectionsAnimation())
                 }
-
+                
                 for (source, target) in changeset.sectionMoved {
                     moveSection(source, toSection: target)
                 }
-
+                
                 if !changeset.elementDeleted.isEmpty {
                     deleteRows(at: changeset.elementDeleted.map { IndexPath(row: $0.element, section: $0.section) }, with: deleteRowsAnimation())
                 }
-
+                
                 if !changeset.elementInserted.isEmpty {
                     insertRows(at: changeset.elementInserted.map { IndexPath(row: $0.element, section: $0.section) }, with: insertRowsAnimation())
                 }
-
+                
                 if !changeset.elementUpdated.isEmpty {
                     reloadRows(at: changeset.elementUpdated.map { IndexPath(row: $0.element, section: $0.section) }, with: reloadRowsAnimation())
                 }
-
+                
                 for (source, target) in changeset.elementMoved {
                     moveRow(at: IndexPath(row: source.element, section: source.section), to: IndexPath(row: target.element, section: target.section))
                 }
