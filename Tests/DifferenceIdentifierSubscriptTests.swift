eddad6db3a880117c2a8a4efca1dd759a5fffbcf
Wendy Liga <agilydnew@gmail.com>
2020-08-07 18:52:58 +0700

    fix swiftlint

--- Tests/DifferenceIdentifierSubscriptTests.swift
+++ Tests/DifferenceIdentifierSubscriptTests.swift
@@ -6,7 +6,7 @@ final class DifferenceIdentifierSubscriptTests: XCTestCase {
         let differenceIdentifier: String
         var count: Int
     }
-    
+
     func testSubscriptSetterExist() {
         var d1 = [
             Counter(differenceIdentifier: "first", count: 0),
@@ -18,11 +18,11 @@ final class DifferenceIdentifierSubscriptTests: XCTestCase {
             Counter(differenceIdentifier: "second", count: 100),
             Counter(differenceIdentifier: "third", count: 0)
         ]
-        
+
         d1[id: "second"]?.count = 100
         XCTAssertEqual(d1, d2)
     }
-    
+
     func testSubscriptSetterNotExist() {
         var d1 = [
             Counter(differenceIdentifier: "first", count: 0),
@@ -34,28 +34,28 @@ final class DifferenceIdentifierSubscriptTests: XCTestCase {
             Counter(differenceIdentifier: "second", count: 0),
             Counter(differenceIdentifier: "third", count: 0)
         ]
-        
+
         d1[id: "forth"]?.count = 100
         XCTAssertEqual(d1, d2)
     }
-    
+
     func testSubscriptGetterExist() {
         let d1 = [
             Counter(differenceIdentifier: "first", count: 100),
             Counter(differenceIdentifier: "second", count: 0),
             Counter(differenceIdentifier: "third", count: 0)
         ]
-        
+
         XCTAssertEqual(d1[id: "first"]?.count, 100)
     }
-    
+
     func testSubscriptGetterNotExist() {
         let d1 = [
             Counter(differenceIdentifier: "first", count: 0),
             Counter(differenceIdentifier: "second", count: 0),
             Counter(differenceIdentifier: "third", count: 0)
         ]
-        
+
         XCTAssertEqual(d1[id: "forth"], nil)
     }
 }
