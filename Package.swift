19d7e51f3358b6a76c0014bb89955e687e1db71c
417-72KI <417.72ki@gmail.com>
2024-01-19 17:41:42 +0900

    Add Privacy Manifest

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
@@ -13,7 +13,8 @@ let package = Package(
     targets: [
         .target(
             name: "DifferenceKit",
-            path: "Sources"
+            path: "Sources",
+            resources: [.copy("PrivacyInfo.xcprivacy")]
         ),
         .testTarget(
             name: "DifferenceKitTests",
